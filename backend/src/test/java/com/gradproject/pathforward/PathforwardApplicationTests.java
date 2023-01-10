78b35049fecf5a145d87b0cd3b6f9296d0e28373
Anders Myrman <amyhrman@gmail.com>
2023-01-10 18:47:29 +0100

    PF-5 Create initial dir structure

---
+++ backend/src/test/java/com/gradproject/pathforward/PathforwardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gradproject.pathforward;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathforwardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
