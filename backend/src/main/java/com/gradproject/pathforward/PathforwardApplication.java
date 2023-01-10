533d9d39d5d43f38f3ddb290cfa1d95e0437a284
Anders Myrman <amyhrman@gmail.com>
2023-01-10 18:47:29 +0100

    PF-5 Create initial dir structure

---
+++ backend/src/main/java/com/gradproject/pathforward/PathforwardApplication.java
@@ -0,0 +1,13 @@
+package com.gradproject.pathforward;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathforwardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathforwardApplication.class, args);
+	}
+
+}
