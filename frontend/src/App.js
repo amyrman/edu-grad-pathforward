535c03d403b1268b5fc1f222013a12a90ec3bade
Anders Myrman <amyhrman@gmail.com>
2023-01-30 23:10:24 +0100

    Create loginform

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,44 +1,40 @@
-import axios from "axios";
-import React from "react";
-import './App.css';
+import React, { Component } from 'react';
+import LoginForm from './component/LoginForm.js';
 
-const baseURL = "http://localhost:8080/users/1";
-
-function App() {
-  const [post, setPost] = React.useState(null);
-
-  React.useEffect(() => {
-    axios.get(baseURL).then((response) => {
-      setPost(response.data);
-    });
-  }, []);
-
-  if (!post) return null;
-
-  return (
-    <div>
-      <h1>{post.name}</h1>
-      <p>{post.email}</p>
-    </div>
-  );
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <LoginForm />
+      </div>
+    );
+  }
 }
 
 export default App;
+
+// import axios from "axios";
+// import React from "react";
+// import './App.css';
+
+// const baseURL = "http://localhost:8080/users/1";
+
+// function App() {
+//   const [post, setPost] = React.useState(null);
+
+//   React.useEffect(() => {
+//     axios.get(baseURL, {
+//       headers: {
+//         "Authorization": "Bearer eyJraWQiOiIxIiwiYWxnIjoiRVMyNTYifQ.eyJzdWIiOiJ0ZXN0QHRlc3QubnUiLCJpc3MiOiJodHRwczovL2Z1bmdvdmVyLm9yZy9hdXRoIiwiaWF0IjoxNjc1MDc5NDA0LCJleHAiOjE2NzUwODAzMDQsImF1ZCI6Imh0dHBzOi8vZnVuZ292ZXIub3JnIiwic2NvcGUiOiJyZWFkIHdyaXRlIiwicm9sZXMiOlsicmVhZCIsIndyaXRlIl0sInZlaGljbGVzIjpbIjEiLCIyIl19.zLnVFXclqvQrgiqzc1xRCLVWvGeTm0hNXQ6OxQYqC8GlK1K6vqfvO-eoMqrjiDsmdMaX6Wjym2I4M4Bd7NXmMg"}}).then((response) => {
+//       setPost(response.data);
+//     });
+//   }, []);
+
+//   if (!post) return null;
+
+//   return (
+//     <div>
+//       <h1>{post.name}</h1>
+//       <p>{post.email}</p>
+//     </div>
+//   );
---
+++ frontend/src/component/LoginForm.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { Label, Form, FormGroup, Input, Button } from "reactstrap";
+import axios from "axios";
+
+import "../css/LoginForm.css";
+
+const LoginForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post("https://fungover.org/auth", { username, password })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  return (
+    <>
+    <div className="LoginForm">
+      <h2>Log In</h2>
+      <Form className="form" onSubmit={handleSubmit}>
+        <FormGroup floating>
+          <Input
+            id="exampleEmail"
+            name="email"
+            placeholder="Email"
+            type="email"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+          <Label for="exampleEmail">Email</Label>
+        </FormGroup>
+        {" "}
+        <FormGroup floating>
+          <Input
+            id="examplePassword"
+            name="password"
+            placeholder="password"
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <Label for="examplePassword">Password</Label>
+        </FormGroup>
+        <Button type="submit">Submit</Button>
+      </Form>
+      </div>
+    </>
+    
+  );
+};
+
+export default LoginForm;
