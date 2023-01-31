543b417142bb33fd692723dc8509a5d4b2ce7fee
Anders Myrman <amyhrman@gmail.com>
2023-01-31 11:18:24 +0100

    Clean up App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,14 @@
 import React, { Component } from 'react';
 import LoginForm from './component/LoginForm.js';
+import Profile from './component/Profile.js';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <LoginForm />
-      </div>
-    );
-  }
-}
+function App() {
+  return (
+    <div className="App">
+      <LoginForm />
+      <Profile />
+    </div>
+  );
+};
 
 export default App;
-
-// import axios from "axios";
-// import React from "react";
-// import './App.css';
-
-// const baseURL = "http://localhost:8080/users/1";
-
-// function App() {
-//   const [post, setPost] = React.useState(null);
-
-//   React.useEffect(() => {
-//     axios.get(baseURL, {
-//       headers: {
-//         "Authorization": "Bearer eyJraWQiOiIxIiwiYWxnIjoiRVMyNTYifQ.eyJzdWIiOiJ0ZXN0QHRlc3QubnUiLCJpc3MiOiJodHRwczovL2Z1bmdvdmVyLm9yZy9hdXRoIiwiaWF0IjoxNjc1MDc5NDA0LCJleHAiOjE2NzUwODAzMDQsImF1ZCI6Imh0dHBzOi8vZnVuZ292ZXIub3JnIiwic2NvcGUiOiJyZWFkIHdyaXRlIiwicm9sZXMiOlsicmVhZCIsIndyaXRlIl0sInZlaGljbGVzIjpbIjEiLCIyIl19.zLnVFXclqvQrgiqzc1xRCLVWvGeTm0hNXQ6OxQYqC8GlK1K6vqfvO-eoMqrjiDsmdMaX6Wjym2I4M4Bd7NXmMg"}}).then((response) => {
-//       setPost(response.data);
-//     });
-//   }, []);
-
-//   if (!post) return null;
-
-//   return (
-//     <div>
-//       <h1>{post.name}</h1>
-//       <p>{post.email}</p>
-//     </div>
-//   );
